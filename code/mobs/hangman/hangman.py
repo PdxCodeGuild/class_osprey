9080594101c8731f6b6501d7ca09e7fa7dd53886
LiamDes <deshaiesash@gmail.com>
2023-03-09 11:37:29 -0800

    done base

--- code/mobs/hangman/hangman.py
+++ code/mobs/hangman/hangman.py
@@ -15,59 +15,60 @@ between the guess counter and guessed letters need to ask user to imput a letter
 '''
 
 
-# def load_words():
-#     with open('code/mobs/hangman/english.txt', 'r') as file:
-#         words = file.read().split('\n')
+def load_words():
+    with open('code/mobs/hangman/english.txt', 'r') as file:
+        words = file.read().split('\n')
 
-#     choices = []
+    choices = []
 
-#     for word in words:
-#         if len(word) >= 5:
-#             choices.append(word)
-#     # print(choices)
-#     return(choices)
+    for word in words:
+        if len(word) >= 5:
+            choices.append(word)
+    # print(choices)
+    return(choices)
 
-# choices = load_words()
-# word_to_guess = random.choice(choices)
-# print(word_to_guess)
-
-word_to_guess = 'otter'
-
-holder = []
-
-for letter in range(len(word_to_guess)):
-    holder.append('_') 
-blanks = ' '.join(holder)
-
-print(blanks)
-
-
-user_guesses = []
-guess_count = 10
-
+choices = load_words()
 
 while True: 
-    guess = input("guess a letter: ").lower()
+    holder = []
+    user_guesses = []
+    guess_count = 10
+    word_to_guess = random.choice(choices)
+    for letter in range(len(word_to_guess)):
+        holder.append('_') 
+        blanks = ' '.join(holder)
+
+    print(blanks)
+
+    while True:
+        guess = input("guess a letter: ").lower()
    
-    if guess in word_to_guess:
-        user_guesses.append(guess)
-        for match in range(len(holder)):
-            for letter in range(len(word_to_guess)):
-                if guess == word_to_guess[letter]:
-                    holder[letter] = guess
-                
-    
-    elif guess in string.ascii_letters:
-        user_guesses.append(guess)
-        guess_count -= 1
-    else:
-        print("invalid guess.")
-        continue
-    print(user_guesses, holder, guess_count)
-    if guess_count == 0:
-        print(f'you lost, the word was {word_to_guess}')
-        play_again = input("Would you like to play again?")
-        if play_again == 'y':
-            continue # TODO we need to move our random choice to game loop
+        if guess in word_to_guess:
+            user_guesses.append(guess)
+            for match in range(len(holder)):
+                for letter in range(len(word_to_guess)):
+                    if guess == word_to_guess[letter]:
+                        holder[letter] = guess
+                    
+        
+        elif guess in string.ascii_letters:
+            user_guesses.append(guess)
+            guess_count -= 1
         else:
+            print("invalid guess.")
+            continue
+        print(f'''{holder}
+guesses: {set(user_guesses)} 
+guesses remaining: {guess_count}
+''')
+        if list(word_to_guess) == holder:
+            print('you win!')
             break
+        if guess_count == 0:
+            print(f'you lost, the word was {word_to_guess}')
+            break
+    play_again = input("Would you like to play again?")
+    if play_again == 'y':
+        continue # TODO we need to move our random choice to game loop
+    else:
+        break
