4b103be306b3a886d9e796891d1e249d946753e3
Cynthia S <c.sovulewski@gmail.com>
2023-03-10 12:16:54 -0800

    changes to lab07 and csv, not complete

--- code/cynthia/lab07.3.py
+++ code/cynthia/lab07.3.py
@@ -3,9 +3,10 @@
 
 #implement a CRUD REPL
 
-with open('code/cynthia/addess_book.csv', 'r') as file:
-    text = file.read().split('\n')
-
+with open('code/cynthia/addess_book.csv', 'r') as friends_list_file:
+    text = friends_list_file.read().split('\n')
+    text[0] #becomes headers
+    
 details =[]
 friends_list = []
 
@@ -21,56 +22,68 @@ print(f'Your friends are {friends_list}')
 
 
 while True:
-    create = input('Would you like to add a new friend?:')
-    if create != 'y':
+
+    make_changes = input('''
+Enter 'create', 'retrieve', 'update', 'delete' or none to exit. 
+Enter the change you would like to make:
+''')
+    if make_changes == 'none':
+        print(friends_list)
         break
 
-    while create == 'y':
-        new_friend = input("new friends name: ")
+    add_friends_list = []
+    if make_changes == 'create':
+        new_friend = input("new friends name: ").capitalize()
         new_color = input("their favorite color: ")
         new_fruit = input("their favorite fruit: ")
         add_friends_list=[{'name': new_friend, 'favorite color': new_color, 'favorite fruit':new_fruit}]
-        break
-    friends_list.extend(add_friends_list)
-    print(friends_list)
+        
+        friends_list.extend(add_friends_list)
+        print()
+        print(f'Your updated friends are: {friends_list}')
+
 
     friend_info = []
 
-    retrieve = input('Would you like to retrieve friend?:')
-    while retrieve != 'y':
-        break
-    info = input('Name of friend for info: ')
-    
-    for i in range(len(friends_list)):
-        if friends_list[i]['name'] == info:
-            friend_info.append(friends_list[i])
-            break
-    print(friend_info)
+    if make_changes == 'retrieve':
+        info = input('Name of friend you want to retrieve: ').capitalize()
+        for i in range(len(friends_list)):
+            if friends_list[i]['name'] == info:
+                friend_info.append(friends_list[i])
+        print()
+        print(f"{info}'s information is {friend_info}")
 
     
-    update = input("Would you like to update a friend? ")
-    while update != 'y':
-        break
-    friend_to_update = input('Name of the friend you want to update: ')
-    attribute_key= input('What attribute do you want to update? ')
-    new_attribute = input('New attribute: ')
+    if make_changes == 'update':
+        friend_to_update = input('Name of the friend you want to update: ').capitalize()
 
-    for i in range(len(friends_list)):
-        if friends_list[i]['name'] == friend_to_update:
-            friends_list[i][attribute_key] = new_attribute
-    print(friends_list)
+        attribute_key= input('''Choose from "name", "favorite color", or "favorite fruit".
+What information do you want to update? ''')
+                             
+        new_attribute = input('New attribute: ')
+        for i in range(len(friends_list)):
+            if friends_list[i]['name'] == friend_to_update:
+                friends_list[i][attribute_key] = new_attribute
+        print()        
+        print(f'Your updated friend is: {friends_list}')
 
-    # delete = input("Would you like to delete a friend? ")
-    # while delete != 'y':
-    #     break
-    # delete_a_friend = input("What is the name of the person to remove? ")
-    # for i in range(len(friends_list)):
-    #     if friends_list[i]['name'] == delete_a_friend:
-    #         del friends_list[i]['name']
-    #         break
-    # print(friends_list)
+    if make_changes == 'delete':
+        delete_a_friend = input("What is the name of the person to remove? ").capitalize()
+        for i in range(len(friends_list)):
+            if friends_list[i]['name'] == delete_a_friend:
+                del friends_list[i]
+                break
+        print()
+        print(f"Your friends list is now: {friends_list}")
 
 
+    lines = []
+    comma = ','
 
-with open('code/cynthia/addess_book.csv', 'a') as file:
-        file.write(friends_list)
+    with open('code/cynthia/addess_book.csv', 'a') as friends_list_file:
+            for line in friends_list:
+                lines = f'\n{line}\n'
+                friends_list_file.write(lines)
+
+
+       
