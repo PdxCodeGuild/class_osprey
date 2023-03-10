7b9cdddfcbffa9a178cfec4cd31534a697280a0d
Cynthia S <c.sovulewski@gmail.com>
2023-03-09 16:36:52 -0800

    final version of lab07.2

--- code/cynthia/lab07.2.py
+++ code/cynthia/lab07.2.py
@@ -17,43 +17,54 @@ for line in text:
     favorite_fruit = details[2]
 
     friends_list.append({'name': name, 'favorite color':favorite_color, 'favorite fruit':favorite_fruit})
-print(friends_list)
+print(f'Your friends are {friends_list}')
 
-# create = input('Would you like to add a new friend?:')
-# create = 'y'
 
-# while create == 'y':
-# new_friend = input("new friends name: ")
-# new_color = input("their favorite color: ")
-# new_fruit = input("their favorite fruit: ")
-# add_friends_list=[{'name': new_friend, 'favorite color': new_color, 'favorite fruit':new_fruit}]
-
-# friends_list.extend(add_friends_list)
-# print(friends_list)
-
-friend_info = []
-# # retrieve = input('Would you like to retrieve friend?:')
-# # retrieve = 'y'
-
-# # while retrieve == 'y':
-# info = input('Name of friend for info: ')
-
-# for i in range(len(friends_list)):
-#     if friends_list[i].get('name') == info:
-#         friend_info.append(friends_list[i])
-#         break
-# print(friend_info)
-
-friend_to_update = input('Name of the friend you want to update: ')
-attribute_key= input('What attribute do you want to update? ')
-new_attribute = input('New attribute: ')
-
-for i in range(len(friends_list)):
-    if friends_list[i].get('name') == friend_to_update:
-        if new_attribute == 'favorite color':
-            friends_list['favorite color'] = new_attribute
-        elif new_attribute == 'favorite fruit':
-            friends_list['favorite fruit'] = new_attribute
-        friends_list.append(friends_list[i])
+while True:
+    create = input('Would you like to add a new friend?:')
+    if create != 'y':
         break
-print(friend_info)
+
+    while create == 'y':
+        new_friend = input("new friends name: ")
+        new_color = input("their favorite color: ")
+        new_fruit = input("their favorite fruit: ")
+        add_friends_list=[{'name': new_friend, 'favorite color': new_color, 'favorite fruit':new_fruit}]
+        break
+    friends_list.extend(add_friends_list)
+    print(friends_list)
+
+    friend_info = []
+
+    retrieve = input('Would you like to retrieve friend?:')
+    if retrieve != 'y':
+        break
+    info = input('Name of friend for info: ')
+
+    for i in range(len(friends_list)):
+        if friends_list[i]['name'] == info:
+            friend_info.append(friends_list[i])
+            break
+    print(friend_info)
+
+
+    update = input("Would you like to update a friend? ")
+    if update != 'y':
+        break
+    friend_to_update = input('Name of the friend you want to update: ')
+    attribute_key= input('What attribute do you want to update? ')
+    new_attribute = input('New attribute: ')
+
+    for i in range(len(friends_list)):
+        if friends_list[i]['name'] == friend_to_update:
+            friends_list[i][attribute_key] = new_attribute
+    print(friends_list)
+
+    delete = input("Would you like to delete a friend? ")
+    if delete != 'y':
+        break
+    delete_a_friend = input("What is the name of the person to remove? ")
+    for i in range(len(friends_list)):
+        if friends_list[i]['name'] == delete_a_friend:
+            del friends_list[i]
+    print(friends_list)
