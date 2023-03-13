53df44fbaa421c30a15d5b5dd7c60712caea0ff5
Cynthia S <c.sovulewski@gmail.com>
2023-03-13 16:10:20 -0700

    finished lab08.1

--- code/cynthia/lab08.1.py
+++ code/cynthia/lab08.1.py
@@ -3,10 +3,33 @@
 #two attributes: balance and interest rate
 
 
+
 class ATM:
 
-    def __init__(self, atm):
-        self.atm = ATM()
+
+    def __init__(self, balance = 0) -> None:
+        self.balance = balance
+        self.interest_rate = .1
+
+    def check_balance(self): #returns the account balance
+        return self.balance
+
+    def deposit(self, amount): #deposits the given amount in the account
+        self.balance = amount + self.balance 
+        return self.balance
+
+    def check_withdrawal(self, amount): #returns true if the withdrawn amount wont put the account in the nevative
+        if self.balance - amount >= 0:
+            return True
+
+    def withdraw(self, amount): #withdraws the amount from the account and returns it
+        self.balance = self.balance - amount
+
+    def calc_interest(self): #returns the amount of interest calculated on the account
+        amount = self.balance * self.interest_rate
+        return amount 
+
+
 
 atm = ATM() # create an instance of our class
 print('Welcome to the ATM')
@@ -41,25 +64,5 @@ while True:
         break
     else:
         print('Command not recognized') 
-    balance = 0
-    interest_rate = .1
 
-
-
-    def check_balance(self, balance): #returns the account balance
-        self.balance = balance
-        pass
-
-    def deposit(self, amount): #deposits the given amount in the account
-        self.amount = balance - amount 
-        pass
-
-    def check_withdrawal(amount): #returns true if the withdrawn amount wont put the account in the nevative
-        pass
-
-    def withdraw(amount): #withdraws the amount from the account and returns it
-        pass
-
-    def calc_interest(): #returns the amount of interest calculated on the account
-        pass 
 
