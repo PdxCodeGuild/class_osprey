2b6eadcd0c3223f166b68887f79a058492abbeee
Cynthia S <c.sovulewski@gmail.com>
2023-03-14 16:33:02 -0700

    updates to lab09

--- code/cynthia/Python/lab09.py
+++ code/cynthia/Python/lab09.py
@@ -4,10 +4,9 @@ class Compass:
     def __init__(self, heading) -> None:
         self.heading = heading
         
-
     def get_direction(self) -> str:
         '''Find the nearest cardinal direction of the current heading'''
-        direction = round(self.heading/360)*45
+        direction = degrees
 
         match direction:
             case 0:
@@ -26,24 +25,34 @@ class Compass:
                 return 'W'
             case 315:
                 return 'NW'
-    
+        return direction
 
     def turn(self, degrees, direction) -> None:
         ...
+
         print(
             f'Turned {degrees} degrees {direction}. New heading is {self.heading}, pointed roughly {self.get_direction()}')
 
     def __add__(self, degrees: int) -> int:
         '''Add two different compass headings together'''
-        ...
+        degrees = ((new_heading + self.heading)/45)*45
+        if degrees >= 359:
+            degrees = round(heading / 45)* 45%45 
+        return degrees
 
     def __sub__(self, degrees: int) -> int:
-        '''Subract one compass heading from another'''
-        ...
+        '''Subract one compass heading from another''' #keeps mimicking the first headings degree output
+        degrees = ((new_heading - self.heading)/45)*45
+        if degrees <= 0:
+            degrees = ((new_heading - self.heading)/45)* 45% 45
+        return degrees 
 
     def __eq__(self, other) -> bool:
         '''Determine whether two compasses have the same heading'''
-        ...
+        if self.heading == other:
+            print(f'{self.heading} is the same as {other}') #needs to be refined 
+        
+
 
     def __gt__(self, other):
         raise TypeError('Cardinal directions cannot be greater or less than')
@@ -54,4 +63,21 @@ class Compass:
     def __str__(self) -> str:
         ...
 
-while __name__ in "__main__':
+
+if __name__ in '__main__':
+
+    heading = int(input('heading: '))
+    degrees = round(heading / 45)* 45
+    compass = Compass(heading)
+    if heading >= 359:
+        degrees = round(heading / 45)* 45%45  #trying to make the compass cyclical
+    degrees = compass.get_direction()
+    print(degrees)
+
+
+    new_heading = int(input('New heading: '))
+    compass.__add__(degrees)
+    print(degrees)
+
+    other = int(input("what is the other heading: "))
+    compass.__eq__(other)
