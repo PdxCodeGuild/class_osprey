22a1c733a78ed4512804400398aea86458ec8ec2
Danny Burrow <danny@pdxcodeguild.com>
2023-03-14 15:15:05 -0700

    reorganize

--- code/cynthia/Python/lab07.1.py
+++ code/cynthia/Python/lab07.1.py
@@ -1,9 +1,15 @@
+#lab07.1
+#without using csv import
 
-address_book = {}
 
-with open('code/cynthia/address_book_labo7.csv', 'w') as file:
-    lines = file.read().split('\n')
-    address_book = file.read()
-    address_book = {rows[0]:rows[0] for rows in address_book}
-print(address_book)
+with open('code/cynthia/addess_book.csv', 'r') as friends_list_file:
+    text = friends_list_file.read().split('\n')
+    lines = text[1:]
+    keys = text[0].split(',')
 
+friends_list = []
+
+for line in lines:
+    individuals = line.split(',')
+    friends_list.append({keys[0]: individuals[0], keys[1]:individuals[1], keys [2]: individuals[2]})
+print(f'Your friends are {friends_list}')
