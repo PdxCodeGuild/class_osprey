190b91ce42ee9da556952f2c761f36eca1fe23cb
Cynthia S <c.sovulewski@gmail.com>
2023-03-15 10:19:12 -0700

    finished lab10

--- code/cynthia/Python/lab10.2.py
+++ code/cynthia/Python/lab10.2.py
@@ -5,15 +5,16 @@ import time
 #version 2: allow the user to search for jokes
 
 query_params = {}
-query_params["search_term"] = input('')
+query_params["term"] = input('')
 print(query_params)
 
 
-response = requests.get(f'https://icanhazdadjoke.com/', params=query_params, headers = {'accept':'application/json'})
+response = requests.get(f'https://icanhazdadjoke.com/search', params=query_params, headers = {'accept':'application/json'})
 
 # print(response.text)
 
 response_text = json.loads(response.text)
-joke = response_text.get('joke')
-#time.sleep(2)
+# print(response_text)
+joke = response_text.get('results')[0]['joke']
+time.sleep(2)
 print(joke)
