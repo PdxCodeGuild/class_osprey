75800973fcf3f698cc44030e165042d3310a737c
Cynthia S <c.sovulewski@gmail.com>
2023-03-16 14:53:44 -0700

    updates, incomplete

--- code/cynthia/Python/lab11.2.py
+++ code/cynthia/Python/lab11.2.py
@@ -12,12 +12,17 @@ print(query_params)
 
 response = requests.get('https://favqs.com/api/quotes/',params = query_params , headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'})
 
-quote_details = json.loads(response.text)
-pprint.pp(quote_details) 
+page_details = json.loads(response.text)
+# pprint.pp(page_details) #reutnrs all details, including text and author
 
-# body = quote_details['quote']['body']
-# author = quote_details['quote']['author']
 
-# print(f'{body}  - {author}')
+#only return text and author
+body = page_details['quotes'][:]
+just_quotes = []
+for value in body:
+    just_quotes.append(value['body'])
+    just_quotes.append(value['author'])
+pprint.pp(just_quotes)
 
-#repl
+
+
