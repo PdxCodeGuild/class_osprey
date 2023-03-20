f3b6e20a99c87e879e95e68be7506a659efbea6f
Cynthia S <c.sovulewski@gmail.com>
2023-03-20 09:55:33 -0700

    initial file creation

---
+++ code/cynthia/Python/lab13_minicapston.py
@@ -0,0 +1,6 @@
+#Lab13 Minicapstone
+
+#Main Goal: quantify data for an activity and display it in the terminal
+#Extra Goal: allow the user to input the date range or specific activity they want to filter by
+#Stretch goal: colorize and theme the terminal display in the sellected table
+
