f12cd65c6da37e5029f40527429faa74dca1a8e6
Cynthia S <c.sovulewski@gmail.com>
2023-03-22 14:18:50 -0700

    quick save, incomplete

--- code/cynthia/Python/lab13_minicapstone.py
+++ code/cynthia/Python/lab13_minicapstone.py
@@ -5,25 +5,12 @@
 #Extra Goal: allow the user to input the date range or specific activity they want to filter by
 #Stretch goal: colorize and theme the terminal display in the sellected table
 
-#install pretty table -- done
-#import csv
-#parse csv into a listed dictionary
-#display the data in the terminal with a table
-
-
 
 # from prettytable import from_csv
 from prettytable import PrettyTable
+from prettytable.colortable import ColorTable, Theme
 from collections import Counter
-# from datetime import datetime
 import datetime
-
-
-#inporting file, this also prints all data in the csv
-# with open ("code/cynthia/Python/Garmin_activity_data.csv") as fp:
-#     mytable = from_csv(fp)
-# print(mytable)
-
 
 #isolating data to print in the table, trying to import csv, select data then put it into pretty print
 with open('code/cynthia/Python/Garmin_activity_data.csv', 'r') as garmin_data:
@@ -31,29 +18,31 @@ with open('code/cynthia/Python/Garmin_activity_data.csv', 'r') as garmin_data:
     lines = text[1:]
     keys = text[0].split(',')
 
-
-# isolating key information, creating headers for each key, might not use because pretty print can also intake a key later in the add_column statement
-# key_data = keys[0], keys[1], keys[3], keys[4], keys[6]
-
-
 #compiling activity types and occurances
 activity_type = []
 for line in lines:
     columns = line.split(',')
     activity_type.append(columns[0]) #what the activity is named with no repeates
-items_occurance = Counter(activity_type).values()
-items_type = Counter(activity_type).keys()
+items_occurance = list(Counter(activity_type).values())
+items_type = list(Counter(activity_type).keys())
 
-#compiling all distance and time data
-activity_and_distance = []
+#compiling all distance 
 for line in text[1:]:
     total_distance = []
     distance_count = dict.fromkeys(items_type, 0)
 for line in lines:
     text = line.split(',')
     distance_count[text[0]]+= round(float(text[4]))
+total_distance = []
+total_distance = list(distance_count.values())
+total_distance
+
+#average distance
+average_distance = [a/b for a, b in zip(total_distance, items_occurance)]
+rounded_average_distance = [round(num, 1) for num in average_distance]
 
 
+#compiling activity durations
 time_count = dict.fromkeys(items_type, datetime.timedelta())
 for line in lines:
     text = line.split(',')
@@ -68,23 +57,35 @@ for key in time_count:
     days_in_hours = time_count[key].days * 24
     hours = days_in_hours + int(h)
     durations[key] = ':'.join((str(hours),m,s))
-    print(durations)
+just_durations_list = []
+just_durations_list = list(durations.values())
 
 
 
 
-#converting datetime object bask to strings for list, strf 
+#PrettyTable Theme changes
 
-# print(distance_count)
-# print(time_count)
+class Themes:
+    MyTheme = Theme(
+        default_color= '\033[35m',
+        vertical_color="\033[36m",
+        horizontal_color="\033[36m",
+        junction_color="\033[33m",
+        junction_char = '*',
+       
+    )
+type_of_distance = ['mi', 'mi', 'mi', 'mi'
 
-total_distance = []
-total_distance = list(distance_count.values())
-
-#compiling data to be in pretty print terminal display
+#compiling data to be in pretty table terminal display
 x = PrettyTable()
-x.add_column('Activity Type', list(items_type))
-x.add_column('Activity Occurance', list(items_occurance))
+x = ColorTable(theme=Themes.MyTheme)
+
+x.add_column('Activity Type', items_type)
+x.add_column('Activity Occurance', items_occurance)
 x.add_column('Total Distance', total_distance)
-# x.add_column('Total Duration', 0)
+x.add_column('Total Duration', just_durations_list )
+x.add_column("Average Distance", rounded_average_distance)
+x.header_style = 'upper'
+x.sortby = 'Activity Occurance'
+x.reversesort = True 
 print(x)
