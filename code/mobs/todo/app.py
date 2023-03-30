694acaa9240b402132136ecc5cb6b66fad79c674
Cynthia S <c.sovulewski@gmail.com>
2023-03-30 16:26:31 -0700

    done!

--- code/mobs/todo/app.py
+++ code/mobs/todo/app.py
@@ -8,16 +8,32 @@ with open('db.json', 'r') as file:
 
 @app.route('/', methods=['GET'])
 def index():
-
-    
     return render_template('index.html', todos=todos['todos'])
+
 
 @app.route('/todo/', methods=['POST'])
 def todo_list():
-
-    todos['todos'].append(request.form)
+    data=dict(request.form)
+    data['is-done']='no'
+    if len(todos['todos']) == None:
+         data['id'] = 1
+    else:
+        data['id']= todos['todos'][-1]["id"] +1
+        
+    todos['todos'].append(data)
     with open('db.json', 'w') as file:
           file.write(json.dumps(todos, indent=4))
     return redirect('/')
+
+@app.route('/finished/<int:id>', methods = ['POST'])
+def finished_task(id):
+    data=dict(todos)
+    match = list(filter(lambda todo: todo['id'] == id, data['todos']))[0]
+    # x = data['todos'].index(match)
+    match['is-done']='yes' 
+    with open('db.json', 'w') as file:
+          file.write(json.dumps(data, indent=4))
+    return redirect('/')   
+
 
 app.run(debug=True)
