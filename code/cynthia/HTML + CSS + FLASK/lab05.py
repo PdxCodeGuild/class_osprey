a09df497c853355f73d5df0eab6934516d762c8b
Cynthia S <c.sovulewski@gmail.com>
2023-03-30 19:14:57 -0700

    'some changes'

--- code/cynthia/HTML + CSS + FLASK/lab05.py
+++ code/cynthia/HTML + CSS + FLASK/lab05.py
@@ -6,26 +6,35 @@ from number_conversion import convert_number
 
 app = Flask(__name__)
 
-convert_number()
 
+# with open('data.json', 'r') as file:
+#     number = json.load(file)
 
 
 @app.route('/', methods=['GET'])
-def number_conversion():
-    number = x
-    return render_template('lab05.html', number=number['enter_number'] )
+def index():
+    return render_template('index.html')
 
 
-@app.route('/submit-number', methods=["POST"])
-def submit():
-    number['enter-number'].append(request.form)
-    with open('code/cynthia/HTML + CSS + FLASK/lab05.json', 'w') as file:
-        file.write(json.dumps(number))
-    return redirect('/')
 
-with open('code/cynthia/HTML + CSS + FLASK/lab05.json', 'r') as file:
-    number = json.load(file)
+    number = convert_number()
+# @app.route('/number/', methods=['POST'])
+# def number_list():
+#     convert_number('enter-number')
+#     number= dict(request.form)
+#     number['number'].append(number)
+#     # with open('data.json', 'w') as file:
+#     #     file.write(json.dumps(number))
+#     return redirect('/')
 
+
+
+# @app.route('/submit-number', methods=["POST"])
+# def submit():
+#     number['enter-number'].append(request.form)
+#     with open('data.json', 'w') as file:
+#         file.write(json.dumps(number))
+#     return redirect('/')
 
 
 app.run(debug=True)
