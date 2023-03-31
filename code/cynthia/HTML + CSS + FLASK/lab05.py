f6136fcbd48dfba936347004c8a3803cf183d9f3
Cynthia S <c.sovulewski@gmail.com>
2023-03-31 13:59:44 -0700

    final changes

--- code/cynthia/HTML + CSS + FLASK/lab05.py
+++ code/cynthia/HTML + CSS + FLASK/lab05.py
@@ -1,40 +1,20 @@
 # lab05 Flask Form Using Python Lab01.2
 
 from flask import Flask, render_template, request, redirect
-import json
+
 from number_conversion import convert_number
 
 app = Flask(__name__)
 
 
-# with open('data.json', 'r') as file:
-#     number = json.load(file)
-
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-
-
-    number = convert_number()
-# @app.route('/number/', methods=['POST'])
-# def number_list():
-#     convert_number('enter-number')
-#     number= dict(request.form)
-#     number['number'].append(number)
-#     # with open('data.json', 'w') as file:
-#     #     file.write(json.dumps(number))
-#     return redirect('/')
-
-
-
-# @app.route('/submit-number', methods=["POST"])
-# def submit():
-#     number['enter-number'].append(request.form)
-#     with open('data.json', 'w') as file:
-#         file.write(json.dumps(number))
-#     return redirect('/')
+    phrase= ''
+    if request.method == 'POST':
+        number = request.form['enter-number']
+        phrase = convert_number(int(number))
+        print(phrase)
+    return render_template('index.html', phrase= phrase)
 
 
 app.run(debug=True)
--- code/cynthia/HTML + CSS + FLASK/number_conversion.py
+++ code/cynthia/HTML + CSS + FLASK/number_conversion.py
@@ -33,10 +33,8 @@ number_conversion = {
 
 }
 
-x = int(input("choose a number: "))
 
-# function here, then import the function into other file
-def convert_number():
+def convert_number(x):
 
     tens_digit = (x % 100)//10 * 10
     ones_digit = x % 10
@@ -45,27 +43,24 @@ def convert_number():
     if x < 100:
 
         if x <= 9:
-            print(number_conversion.get(ones_digit))
+            return number_conversion.get(ones_digit)
 
         elif 11 <= x <= 19:
-            print(number_conversion[x])
+                return number_conversion[x]
 
         elif ones_digit == 0:
-            print((number_conversion.get(tens_digit)))
+            return (number_conversion.get(tens_digit))
         else:
-            print(number_conversion.get(tens_digit) +
-                ' ' + number_conversion.get(ones_digit))
+            return number_conversion.get(tens_digit) +' ' + number_conversion.get(ones_digit)
 
 
     elif x >= 100:
-        tens_digit = (x % 100)//10 * 10
-        print(f'{number_conversion.get(x//100)}' ' hundred', end=" ")
-
         if tens_digit == 10:
-            print(f'{number_conversion[ones_digit + tens_digit]}')
+            return f'{number_conversion.get(x//100)} hundred {number_conversion[ones_digit + tens_digit]}'
 
         elif 20 <= tens_digit and ones_digit == 0:
-            print(number_conversion.get(tens_digit))
+            return f'{number_conversion.get(x//100)} hundred {number_conversion.get(tens_digit)}'
+
         else:
             tens_text = number_conversion.get(tens_digit)
             if tens_digit == 0:
@@ -73,6 +68,8 @@ def convert_number():
             ones_text = number_conversion.get(ones_digit)
             if ones_digit == 0:
                 ones_text = ''
-            print(f'{tens_text} {ones_text}')
+            return f'{number_conversion.get(x//100)} hundred {tens_text} {ones_text}'
 
-convert_number()
+
+ 
+
