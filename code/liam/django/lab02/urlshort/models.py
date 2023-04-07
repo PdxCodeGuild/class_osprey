71b19f2e70e1780f14974490545653013d7ee3ab
LiamDes <deshaiesash@gmail.com>
2023-04-07 09:56:59 -0700

    set up views

--- code/liam/django/lab02/urlshort/models.py
+++ code/liam/django/lab02/urlshort/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from string import ascii_letters, digits
-import random
+
+# from string import ascii_letters, digits
+# import random
 
 class UrlShorten(models.Model):
     full_url = models.CharField('full url', max_length=400)
-    short_url = ''.join(random.choices(ascii_letters + digits, k=6))
+    short_url = 'TEMPID'
 
     def __str__(self) -> str:
         return self.short_url
--- code/liam/django/lab02/urlshort/views.py
+++ code/liam/django/lab02/urlshort/views.py
@@ -4,6 +4,8 @@ from django.template import loader
 
 from .models import UrlShorten
 
+from string import ascii_letters, digits
+import random
 
 def index(request):
     urls = UrlShorten.objects.all()
@@ -12,3 +14,13 @@ def index(request):
         'urls' : urls,
         }
     return HttpResponse(template.render(context, request))
+
+def shorten(request, url_id):
+    ...
+    short_url = ''.join(random.choices(ascii_letters + digits, k=6))
+    pass
+
+def refer(request, url_id):
+    ...
+    # redirect of localhost/<str:shorturl>/ to full url assigned to it
+    pass
