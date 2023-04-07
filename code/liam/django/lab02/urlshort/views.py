f028f29acfb15c96f61801d2dddfe4b8f59e81a9
LiamDes <deshaiesash@gmail.com>
2023-04-07 09:39:31 -0700

    more setup

--- code/liam/django/lab02/urlshort/views.py
+++ code/liam/django/lab02/urlshort/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import UrlShorten
 
 
 def index(request):
-    return HttpResponse("main page")
+    urls = UrlShorten.objects.all()
+    template = loader.get_template('urlshort/index.html')
+    context = { 
+        'urls' : urls,
+        }
+    return HttpResponse(template.render(context, request))
