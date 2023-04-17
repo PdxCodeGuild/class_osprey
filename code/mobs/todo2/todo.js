15aa97043ee1761b53ded57ead65faef07e3d790
LiamDes <deshaiesash@gmail.com>
2023-04-17 12:36:51 -0700

    prog

--- code/mobs/todo2/todo.js
+++ code/mobs/todo2/todo.js
@@ -1,45 +1,52 @@
-
 const submitButton = document.querySelector('input[type="submit"]')
 const submitText = document.querySelector("input[type = 'text']")
 
 const activeList = document.querySelector("#active-list")
 const completedList = document.querySelector('#completed-list')
 
+let itemsArray = localStorage.getItem('submitText') ? JSON.parse(localStorage.getItem('submitText')) : [];
+console.log(itemsArray)
+localStorage.setItem('submitText', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('submitText'));
+console.log(data)
 
-
-
-
-submitButton.addEventListener('click',()=>{
+const addItems = (item) => {
     const listItem = document.createElement('li')
- 
-
-    listItem.innerText = submitText.value
+    listItem.innerText = item.name
     const todoCheck = document.createElement('input')
     listItem.appendChild(todoCheck)
     todoCheck.type = 'checkbox'
     activeList.appendChild(listItem)
 
     todoCheck.addEventListener('change',() => {
+        let index = itemsArray.indexOf(item)
         if (!todoCheck.checked) {
             activeList.appendChild(listItem)
-       
+            item.isDone = false
+            itemsArray.splice(index, 1, item)
+            localStorage.setItem('submitText', JSON.stringify(itemsArray))
         } else{
             completedList.appendChild(listItem)
+            item.isDone = true
+            itemsArray.splice(index, 1, item)
+            localStorage.setItem('submitText', JSON.stringify(itemsArray))
         }
        })
+}
 
-    todoCheck.id = submitText.value
-
+for (item of data) {
+    addItems(item)
+}
+submitButton.addEventListener('click',()=>{
+    const newItem = {
+        name : submitText.value,
+        isDone: false
+    }
+    itemsArray.push(newItem)
+    localStorage.setItem('submitText', JSON.stringify(itemsArray))
+    
+    addItems(newItem)
+    submitText.value = ''
 })
-
-
-
-
-// for (item of AlllistItems){
-//     const todoCheck = document.createElement('input')
-//     todoCheck.type = 'checkbox'
-//     item.appendChild(todoCheck)
-// }
-
 
 
