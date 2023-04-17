b9219d3368395c706ceec91b10548e78989d1c72
Danny Burrow <danny@pdxcodeguild.com>
2023-04-17 14:19:08 -0700

    finished todo listgit status

--- code/mobs/todo2/todo.js
+++ code/mobs/todo2/todo.js
@@ -1,50 +1,49 @@
 const submitButton = document.querySelector('input[type="submit"]')
 const submitText = document.querySelector("input[type = 'text']")
-
 const activeList = document.querySelector("#active-list")
 const completedList = document.querySelector('#completed-list')
 
 let itemsArray = localStorage.getItem('submitText') ? JSON.parse(localStorage.getItem('submitText')) : [];
-console.log(itemsArray)
-localStorage.setItem('submitText', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('submitText'));
-console.log(data)
 
 const addItems = (item) => {
     const listItem = document.createElement('li')
     listItem.innerText = item.name
     const todoCheck = document.createElement('input')
-    listItem.appendChild(todoCheck)
     todoCheck.type = 'checkbox'
-    activeList.appendChild(listItem)
+    todoCheck.checked = item.isDone
+    listItem.appendChild(todoCheck)
 
-    todoCheck.addEventListener('change',() => {
+    if (!item.isDone) {
+        activeList.appendChild(listItem)
+    } else {
+        completedList.appendChild(listItem)
+    }
+
+    todoCheck.addEventListener('change', () => {
         let index = itemsArray.indexOf(item)
         if (!todoCheck.checked) {
             activeList.appendChild(listItem)
-            item.isDone = false
-            itemsArray.splice(index, 1, item)
-            localStorage.setItem('submitText', JSON.stringify(itemsArray))
-        } else{
+        } else {
             completedList.appendChild(listItem)
-            item.isDone = true
-            itemsArray.splice(index, 1, item)
-            localStorage.setItem('submitText', JSON.stringify(itemsArray))
         }
-       })
+        item.isDone = !item.isDone
+        itemsArray.splice(index, 1, item)
+        localStorage.setItem('submitText', JSON.stringify(itemsArray))
+    })
 }
 
-for (item of data) {
+for (item of itemsArray) {
     addItems(item)
 }
-submitButton.addEventListener('click',()=>{
+
+submitButton.addEventListener('click', () => {
     const newItem = {
-        name : submitText.value,
+        name: submitText.value,
         isDone: false
     }
     itemsArray.push(newItem)
     localStorage.setItem('submitText', JSON.stringify(itemsArray))
-    
+
     addItems(newItem)
     submitText.value = ''
 })
