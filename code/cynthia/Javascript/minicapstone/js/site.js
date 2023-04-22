5ba6f637496e4e112f546cff420a4ee40169eb11
Cynthia S <c.sovulewski@gmail.com>
2023-04-22 13:31:54 -0700

    some progress

--- code/cynthia/Javascript/minicapstone/js/site.js
+++ code/cynthia/Javascript/minicapstone/js/site.js
@@ -11,9 +11,10 @@ new Vue({
             userAddress:'',
             message: 'Have a good day',
             x: document.getElementById('location'),
-            lat:'',
-            lng:'',
-
+            latString:'',
+            lngString:'',
+            daysString:'',
+          
         }
         
 
@@ -25,32 +26,42 @@ new Vue({
         getForcast() {
             axios.get('https://api.breezometer.com/pollen/v2/forecast/daily', {
                 params: {
-                    lat: '38.5815719',
-                    lon: '-121.4943996',
-                    days: '1',
+                    lat: this.latString,
+                    lon: this.lngString,
+                    days: this.daysString,
                     key: appKey,
                     metadata: true,
                     plants: true,
                    
                 },
             }).then(response => {this.output = response.data}).catch(err => console.error(err))
+            console.log(this.daysString)
             
         },
 
         getLocation() {
             axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
                 params: {
-                    address: 'sacramento, california',
+                    address: this.userAddress,
                     key: googleKey,
                 },
             }).then(response => {this.googleResult = response.data.results[0].geometry.location}).catch(err => console.error(err))
+           
         },
+
+
         // onChange(e) {
-        //     this.userAddress = this.googleResult.find(l => this.userAddress = l.lat) 
-        //     console.log(userAddress)
+        //     this.userAddress = this.googleResult.find(l => this.userAddress = l.latString) 
+        //     this.userAddress = this.googleResult.find(l => this.userAddress = l.lngString) 
 
         // },
+
+        changeDay(e) {
+            this.daysString = e.target.value
+            
+        },
       
+     
     }
 
 
