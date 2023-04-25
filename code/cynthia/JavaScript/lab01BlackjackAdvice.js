2f8577c2ebc559ffd5a3e01b855e8f3239bdabf6
Cynthia S <c.sovulewski@gmail.com>
2023-04-24 17:39:54 -0700

    fixed blackjack part 1

--- code/cynthia/JavaScript/lab01BlackjackAdvice.js
+++ code/cynthia/JavaScript/lab01BlackjackAdvice.js
@@ -20,14 +20,18 @@ card2 = prompt("What's your second card?")
 card3 = prompt("What's your third card?")
 
 
-const handValue = deck[card1] + deck[card2] + deck[card3]
+let handValue = deck[card1] + deck[card2] + deck[card3]
 
 if (handValue < 17) {
     console.log(handValue, 'hit')
-} else if (17>= handValue <21 ){
+} else if (17 == handValue){
+    console.log(handValue, 'stay') 
+
+} else if (handValue <21){
     console.log(handValue, 'stay')
+
 } else if (handValue == 21) {
-    console.log('Blackjack!')
-} else {
-    console.log("Already Busted")
+    console.log(handValue, 'Blackjack!')
+} else if (handValue > 21){
+    console.log(handValue, "Already Busted")
 }
